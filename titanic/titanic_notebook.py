787dd4565aa287a7f3c8fd1868e2eee5ce4379b6
Christine Madden <Christine Madden>
2020-04-28 21:22:44 -0700

    adding scaling

--- titanic/titanic_notebook.py
+++ titanic/titanic_notebook.py
@@ -7,8 +7,10 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 # Setup project variables and paths
+PLOT_SHOW = False
 PROJECT_NAME = "titanic"
 
 print(f"{PROJECT_NAME.upper()} KAGGLE COMPETITION")
@@ -35,7 +37,7 @@ print("% of women who survived:", rate_women)
 men = train.loc[train.Sex == 'male']["Survived"]
 rate_men = sum(men)/len(men)
 print("% of men who survived:", rate_men)
-
+print(len(train[train["Survived"] == 1]), len(train[train["Survived"] == 0]))
 features = ["Pclass", "Sex", "SibSp", "Parch"]
 le_sex = LabelEncoder()
 le_sex.fit(train["Sex"])
@@ -50,34 +52,38 @@ train["Embarked_Encoded"] = le_emb.transform(train["Embarked"].astype("str"))
 sns.set(style="ticks", color_codes=True)
 g_train = sns.pairplot(train, diag_kind="hist", hue="Survived")
 plt.tight_layout()
-plt.show()
+PLOT_SHOW and plt.show()
+print()
 
 # Split data into test train
-test_ratio = 0.5
+test_ratio = 0.2
 y = train["Survived"]
 X = pd.get_dummies(train[features])
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=42)
 
 print("Train len:", len(X_train), "Test len:", len(X_test))
+print("Features: ", features)
+
+# Preprocessing
+scaler = StandardScaler().fit(X_train)
 
 # Train model and performance
 model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
-model.fit(X_train, y_train)
-y_pred_train = model.predict(X_train)
-y_pred_test = model.predict(X_test)
+model.fit(scaler.transform(X_train), y_train)
+y_pred_train = model.predict(scaler.transform(X_train))
+y_pred_test = model.predict(scaler.transform(X_test))
 
 print("Train accuracy score:", accuracy_score(y_train, y_pred_train))
 print("Test accuracy score:", accuracy_score(y_test, y_pred_test))
-
 # Predict and Save Submission File
 X_submission = pd.get_dummies(submission_data[features])
-y_pred_submission = model.predict(X_submission)
+y_pred_submission = model.predict(scaler.transform(X_submission))
 output = pd.DataFrame({'PassengerId': submission_data.PassengerId, 'Survived': y_pred_submission})
 
 g_output = sns.pairplot(output, diag_kind="hist", hue="Survived")
 plt.tight_layout()
-plt.show()
+PLOT_SHOW and plt.show()
 
 output.to_csv(os.path.join(output_path, f'{PROJECT_NAME}_submission.csv'), index=False)
 print("Your submission was successfully saved!", len(output))
