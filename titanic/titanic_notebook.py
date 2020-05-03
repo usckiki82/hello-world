b89718dd99335b618fd8625afb2d77b760d87f1a
Christine Madden <Christine Madden>
2020-05-03 16:23:02 -0700

    missing data handling initial

--- titanic/titanic_notebook.py
+++ titanic/titanic_notebook.py
@@ -9,6 +9,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+# Screen Setup
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
 # Setup project variables and paths
 PLOT_SHOW = False
 PROJECT_NAME = "titanic"
@@ -30,28 +35,66 @@ submission_data = pd.read_csv(os.path.join(data_path, "test.csv"))
 train = pd.read_csv(os.path.join(data_path, "train.csv"))
 
 # Data information
-women = train.loc[train.Sex == 'female']["Survived"]
+class_label = "Survived"
+output_key = "PassengerId"
+print(sorted(train.columns))
+print(sorted(submission_data.columns))
+
+print("\nPrinting data info...")
+women = train.loc[train.Sex == 'female'][class_label]
 rate_women = sum(women)/len(women)
 print("% of women who survived:", rate_women)
-men = train.loc[train.Sex == 'male']["Survived"]
+men = train.loc[train.Sex == 'male'][class_label]
 rate_men = sum(men)/len(men)
 print("% of men who survived:", rate_men)
-print(len(train[train["Survived"] == 1]), len(train[train["Survived"] == 0]))
+print(len(train[train[class_label] == 1]), len(train[train[class_label] == 0]))
 
-le_sex = LabelEncoder()
-le_sex.fit(train["Sex"])
-# print(list(le_sex.classes_))
-train["Sex_Encoded"] = le_sex.transform(train["Sex"])
-submission_data["Sex_Encoded"] = le_sex.transform(submission_data["Sex"])
-print("sex", train["Sex"].head())
+# Data Cleaning
+print("\nCleaning data ...")
 
-le_emb = LabelEncoder()
-le_emb.fit(train["Embarked"].astype("str"))
-# print(list(le_emb.classes_))
-train["Embarked_Encoded"] = le_emb.transform(train["Embarked"].astype("str"))
-submission_data["Embarked_Encoded"] = le_emb.transform(submission_data["Embarked"].astype("str"))
-print(train.columns.values)
-print(train.head())
+train["Cabin_Sector"] = train["Cabin"].astype(str).str[0]
+submission_data["Cabin_Sector"] = submission_data["Cabin"].astype(str).str[0]
+
+train["Cabin_Local"] = train["Cabin"].astype(str).str[1:]
+submission_data["Cabin_Local"] = submission_data["Cabin"].astype(str).str[1:]
+
+cols_encode = ["Sex", "Embarked", "Cabin_Sector"]
+for encode in cols_encode:
+    le = LabelEncoder()
+    le.fit(train[encode].astype("str"))
+    train[encode + "_Encoded"] = le.transform(train[encode].astype("str"))
+    submission_data[encode + "_Encoded"] = le.transform(submission_data[encode].astype("str"))
+
+# Handle Missing Data
+print("\nHandling missing data...")
+# Deal with missing data fields
+# Drop columns (not desired)
+# Impute with Mean, 0 or mode
+# Impute with Mean and add column indicating imputed rows
+# Number of missing values in each column of training data
+missing_val_count_by_column = (train.isnull().sum())
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
+
+# Imputation
+# my_imputer = SimpleImputer()
+# imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+# imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+
+# Imputation removed column names; put them back
+# imputed_X_train.columns = X_train.columns
+# imputed_X_valid.columns = X_valid.columns
+
+# Feature generation
+#TODO exponential combination
+#TODO automated feature generation
+#TODO Explore differences between train data and submission data
+
+# Print Data
+print("\nTRAINING")
+print(train.describe())
+
+print("\nSUBMISSION")
+print(submission_data.describe())
 
 # Plot data
 sns.set(style="ticks", color_codes=True)
@@ -60,16 +103,19 @@ plt.tight_layout()
 PLOT_SHOW and plt.show()
 print()
 
+# TODO plot histograms
+
 # Feature selection
-features = ["Pclass", "Sex_Encoded", "SibSp", "Parch", 'Embarked_Encoded' ]#"Fare"] # "Age", ]#"Fare"]
-print("Features: ", features)
+features = ["Pclass", "Sex_Encoded", "SibSp", "Parch", "Embarked_Encoded",] # "Cabin_Sector_Encoded"]#"Fare"] # "Age", ]#"Fare"]
+print("Selected Features: ", features)
 
 # Split data into test train
 test_ratio = 0.2
-y = train["Survived"]
+random_state = 42
+y = train[class_label]
 X = pd.get_dummies(train[features])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_ratio, random_state=random_state)
 
 print("Train len:", len(X_train), "Test len:", len(X_test))
 
@@ -77,8 +123,12 @@ print("Train len:", len(X_train), "Test len:", len(X_test))
 scaler = StandardScaler().fit(X_train)
 
 # Train model
-# TODO make this a pipeline
-model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+# TODO make this a pipeline & tune parameters
+# TODO perform random forest for important features
+# TODO try semi supervised learning
+
+# Setting random state forces the classifier to produce the same result in each run
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=random_state)
 model.fit(scaler.transform(X_train), y_train)
 y_pred_train = model.predict(scaler.transform(X_train))
 y_pred_test = model.predict(scaler.transform(X_test))
@@ -89,18 +139,12 @@ y_pred_test = model.predict(scaler.transform(X_test))
 tn, fp, fn, tp = confusion_matrix(y_test, y_pred_test).ravel()
 print("Train accuracy score:", accuracy_score(y_train, y_pred_train))
 print("Test accuracy score:", accuracy_score(y_test, y_pred_test))
-print("Test accuracy2 score:", (tp + tn)/(tp + tn + fp + fn))
-
-
+# print("Test accuracy2 score:", (tp + tn)/(tp + tn + fp + fn))
 
 # Predict and Save Submission File
 X_submission = pd.get_dummies(submission_data[features])
 y_pred_submission = model.predict(scaler.transform(X_submission))
-output = pd.DataFrame({'PassengerId': submission_data.PassengerId, 'Survived': y_pred_submission})
-
-g_output = sns.pairplot(output, diag_kind="hist", hue="Survived")
-plt.tight_layout()
-PLOT_SHOW and plt.show()
+output = pd.DataFrame({output_key: submission_data.PassengerId, class_label: y_pred_submission})
 
 output.to_csv(os.path.join(output_path, f'{PROJECT_NAME}_submission.csv'), index=False)
-print("Your submission was successfully saved!", len(output))
+print("Your submission output was successfully saved!", len(output))
